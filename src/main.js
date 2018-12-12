9b303dc79f4943bc5b568e453fd2dd20a72a00a8
nielsVoogt <niels@branchonline.nl>
2018-12-12 21:41:07 +0100

    Added hammer, anime and pulltorefresh + style.scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,39 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+import './styles/app.scss';
+import PullToRefresh from 'pulltorefreshjs';
+import anime from 'animejs';
+import 'hammerjs';
+
+// ------ Setting up hammer
+
+const mc = new Hammer(document.getElementById('myElement'));
+
+const animateEl = (dir) => {
+	return anime({
+		targets: myElement,
+		translateY: () => {
+			if(dir === 16) {
+				return '+=150';
+			} else if (dir === 8) {
+				return '-=150';
+			}
+		},
+		duration: 300,
+	});
+}
+	
+mc.on("swipedown swipeup", function(ev) {
+	animateEl(ev.direction);
+});
+
+// ---------------------------- LETS SEE HOW THIS WILL GO...
+
+const ptr = PullToRefresh.init({
+  mainElement: '#myElement', // above which element?
+  onRefresh: function (done) {
+    setTimeout(function () {
+      done(); // end pull to refresh
+      alert('refresh');
+    }, 1500);
+  }
+});
