87a5d148654f54bc20cc3b69382f088fd28cddc5
nielsVoogt <niels@branchonline.nl>
2018-12-15 18:24:32 +0100

    Adding some scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
-  <div id="app">
-    hello
-  </div>
+    <div id="app">
+
+        <div class="card card--large-shadow card--quick-select">
+            <div :key="index" class="quick-select-option" v-for="(item, index) in 6">
+                <span class="quick-select-option__icon">
+                    <!-- SVG -->
+                </span>
+                <p class="quick-select-option__description">
+                    Desc
+                </p>
+            </div>
+        </div>
+
+        <p class="title"></p>
+
+        <div class="flex">
+            <div class="card card--balance">
+                <span class="indicator" />
+                <p class="number">1234</p>
+            </div>
+            <div class="card card--balance">
+                <span class="indicator" />
+                <p class="number">1234</p>
+            </div>
+        </div>
+
+
+        <ul class="menu">
+            <li class="menu__item" :key="index" v-for="(item, index) in 4">
+                item
+                <!-- SVG -->
+            </li>
+        </ul>
+
+    </div>
 </template>
 
 <script>
