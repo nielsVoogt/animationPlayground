68a48693fcc55ad2841c144363b4ba8b8f429017
nielsVoogt <niels@branchonline.nl>
2018-12-15 18:08:50 +0100

    Okay, lets build this in vue instead...

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return { }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './styles/app.scss';
+</style>
--- src/main.js
+++ src/main.js
@@ -1,47 +1,56 @@
 
-import './styles/app.scss';
-import PullToRefresh from 'pulltorefreshjs';
-import anime from 'animejs';
-import 'hammerjs';
 
-// ------ Setting up hammer
+import Vue from 'vue'
+import App from './App.vue'
 
-const mc = new Hammer(document.getElementById('myElement'));
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
 
-// ------ Customise these for lr dirs
+// import './styles/app.scss';
+// import PullToRefresh from 'pulltorefreshjs';
+// import anime from 'animejs';
+// import 'hammerjs';
 
-const animateY = (dir) => {
-	return anime({
-		targets: '#myElement',
-		translateY: () => {
-			if(dir === 16) {
-				return '+=150';
-			} else if (dir === 8) {
-				return '-=150';
-			}
-		},
-		duration: 300,
-	});
-}
+// // ------ Setting up hammer
+
+// const mc = new Hammer(document.getElementById('myElement'));
+
+// // ------ Customise these for lr dirs
+
+// const animateY = (dir) => {
+// 	return anime({
+// 		targets: '#myElement',
+// 		translateY: () => {
+// 			if(dir === 16) {
+// 				return '+=150';
+// 			} else if (dir === 8) {
+// 				return '-=150';
+// 			}
+// 		},
+// 		duration: 300,
+// 	});
+// }
 
 
-mc.on("swipeleft swiperight", function(ev) {
-  console.log(ev);
-});
+// mc.on("swipeleft swiperight", function(ev) {
+//   console.log(ev);
+// });
 
-// ---------------------------- LETS SEE HOW THIS WILL GO...
+// // ---------------------------- LETS SEE HOW THIS WILL GO...
 
-const ptr = PullToRefresh.init({
-    mainElement: '#myElement', // above which element?
-    onInit: function() {
-        console.log('refresh started');
-    },
-    ptrElement: 'header',
-    onRefresh: function (done) {
-        setTimeout(function () {
-            done(); // end pull to refresh
-            alert('refresh');
-        }, 1500);
-    }
-});
+// const ptr = PullToRefresh.init({
+//     mainElement: '#myElement', // above which element?
+//     onInit: function() {
+//         console.log('refresh started');
+//     },
+//     ptrElement: 'header',
+//     onRefresh: function (done) {
+//         setTimeout(function () {
+//             done(); // end pull to refresh
+//             alert('refresh');
+//         }, 1500);
+//     }
+// });
 
